ad0fa15e0affa61cc3a2823bfc953ce5b98a8770
unknown <malo@Iqmalo.Iqmalo>
2021-02-25 18:29:53 +0800

    sixth commit, remove border radius

--- src/Components/FindUs/FindUs.js
+++ src/Components/FindUs/FindUs.js
@@ -15,10 +15,10 @@ const FindUs = () => {
                       </div>
                       <div className="btn-container">
                       <div className="btn-wrapper">
-                        <a href="https://goo.gl/maps/6rujrLw1gMPvnexw5"><button className="gmap"><img src={googlemap} alt=""/></button></a>
+                        <a href="https://goo.gl/maps/6rujrLw1gMPvnexw5"><button type="button" className="gmap"><img src={googlemap} alt=""/></button></a>
                       </div>
                       <div className="btn-wrapper2">
-                        <a href="https://ul.waze.com/ul?ll=3.15901600%2C101.62540696&navigate=yes&zoom=17&utm_campaign=default&utm_source=waze_website&utm_medium=lm_share_location"><button className="waze"><img src={waze} alt=""/></button></a>
+                        <a href="https://ul.waze.com/ul?ll=3.15901600%2C101.62540696&navigate=yes&zoom=17&utm_campaign=default&utm_source=waze_website&utm_medium=lm_share_location"><button type="button" className="waze"><img src={waze} alt=""/></button></a>
                       </div>
                       </div>
                   </div>
