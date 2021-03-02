997f02ebb73309245e6103d631d58fd9ef9382d8
unknown <malo@Iqmalo.Iqmalo>
2021-03-02 20:07:50 +0800

    33th commit, add new thanks page to the button in loyalty page, with code for subscribers

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Footer from "./Components/Footer/Footer.js"
 import Loyalty from "./Components/Loyalty/Loyalty.js"
 import Pantry from "./Components/Pantry/Pantry.js"
 import Seni from "./Components/Seni/Seni.js"
-// import Thanks from "./Components/Thanks/Thanks.js"
+import Thanks from "./Components/Thanks/Thanks.js"
 import {BrowserRouter as Router,Route} from "react-router-dom";
 
 
@@ -29,7 +29,7 @@ function App() {
     <Route path = "/" exact component = {Contact} />
     <Route path = "/" exact component = {Loyalty} />
     <Route path="/Seni" component={Seni} />
-    {/* <Route path="/Thanks" exact component={Thanks} /> */}
+    <Route path="/Thanks" exact component={Thanks} />
     <Footer />
     </div>
     </Router>
